5f0d0369f4b51657320d2596307bc566ec886a6f
Lourenço Braga <lbraganeto2003@gmail.com>
2026-07-10 09:01:49 -0400

    using props

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,10 @@ import "./styles/global.css";
 export function App() {
   return (
     <>
-      <Heading />
+      <Heading attr={123} attr2="oss">Brasil</Heading>
+      <Heading>Canarinho</Heading>
+      <Heading>Brasa</Heading>
+      <Heading>Brazuca</Heading>
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint amet
         dignissimos minus harum. Molestias accusantium eveniet quaerat
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,7 +1,7 @@
 import styles from './Heading.module.css'
 
-export function Heading() {
+export function Heading(props) {
     return (
-        <h1 className={styles.heading}>Olá mundo</h1>
+        <h1 className={styles.heading}> {props.children} </h1>
     )
 }
