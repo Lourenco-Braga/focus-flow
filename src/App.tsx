030f8a4faf7f3c2f6c13afe01d6529a3ab729f14
Lourenço Braga <lbraganeto2003@gmail.com>
2026-07-10 10:42:52 -0400

    creating heading

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,18 @@ import { Heading } from "./components/Heading";
 
 import "./styles/theme.css";
 import "./styles/global.css";
+import { Timer } from "lucide-react";
 
 export function App() {
   return (
     <>
-      
+      <Heading>
+        Bonju
+    
+        <button>
+          <Timer /> 
+        </button>
+      </Heading>
     </>
   );
 }
