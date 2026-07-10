1c16c1865f06c126c7163e46947577880ad72325
Lourenço Braga <lbraganeto2003@gmail.com>
2026-07-10 10:17:58 -0400

    creating css variables

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,7 @@ import "./styles/global.css";
 export function App() {
   return (
     <>
-      <Heading attr={123} attr2="oss">Brasil</Heading>
-      <Heading>Canarinho</Heading>
-      <Heading>Brasa</Heading>
-      <Heading>Brazuca</Heading>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint amet
-        dignissimos minus harum. Molestias accusantium eveniet quaerat
-        consectetur corporis voluptas , possimus amet veritatis porro
-        repellendus rerum neque ut ipsa et.
-      </p>
+      
     </>
   );
 }
