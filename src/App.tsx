fd71c36e701638606b2c61cadc04e928fc99b164
Lourenço Braga <lbraganeto2003@gmail.com>
2026-07-10 12:40:58 -0400

    adjusting components files

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
-import { Heading } from "./components/Heading";
-
 import "./styles/theme.css";
 import "./styles/global.css";
-import { Timer } from "lucide-react";
+import { Container } from "./components/Container";
+import { Heading } from "./components/Heading";
+
 
 export function App() {
   return (
     <>
-      <Heading>
-        Bonju
-    
-        <button>
-          <Timer /> 
-        </button>
-      </Heading>
+      <Container> 
+        <Heading>LOGO</Heading>
+      </Container>
+      <Container> 
+        <Heading>MENU</Heading>
+      </Container>
+      <Container> 
+        <Heading>FORM</Heading>
+      </Container>
+      <Container> 
+        <Heading>FOOTER</Heading>
+      </Container>
     </>
   );
 }
