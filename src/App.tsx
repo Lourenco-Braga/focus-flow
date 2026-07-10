60b2fc8a4a3c04e758b5b26e0701a32753f2df80
Lourenço Braga <lbraganeto2003@gmail.com>
2026-07-10 08:52:11 -0400

    heading file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
- import './styles/theme.css'
- import './styles/global.css'
- 
- export function App() {
-  return (
-    <div className="App">
-      <h1></h1>
-    </div>
-  )
-}
+import { Heading } from "./components/Heading";
 
+import "./styles/theme.css";
+import "./styles/global.css";
+
+export function App() {
+  return (
+    <>
+      <Heading />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint amet
+        dignissimos minus harum. Molestias accusantium eveniet quaerat
+        consectetur corporis voluptas , possimus amet veritatis porro
+        repellendus rerum neque ut ipsa et.
+      </p>
+    </>
+  );
+}
