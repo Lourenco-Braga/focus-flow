5b2f10ac283983f4d8a49fef764c6ae37019c15d
Lourenço Braga <lbraganeto2003@gmail.com>
2026-07-13 10:35:22 -0400

    Cycles component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
 import { Countdown } from "./components/Countdown";
 import { DefaultInput } from "./components/DefaultInput";
+import { Cycles } from "./components/Cycles";
 
 export function App() {
   return (
@@ -22,7 +23,7 @@ export function App() {
       <Container>
         <form className="form" action="">
           <div className="formRow">
-            <DefaultInput labelText="Task" id="menuInput" type="text" placeholder="Digite a Task..." />
+            <DefaultInput labelText="Task" id="menuInput" type="text" placeholder="Digite o nome da tarefa..." />
           </div>
 
           <div className="formRow">
@@ -30,8 +31,7 @@ export function App() {
           </div>
 
           <div className="formRow">
-            <p>Ciclos</p>
-            <p>0 0 0 0 0 0</p>
+            <Cycles />
           </div>
 
           <div className="formRow">
