30869868f560f6e6a56ba4806f133df1eb1d93e4
Lourenço Braga <lbraganeto2003@gmail.com>
2026-07-14 10:32:29 -0400

    light theme

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,21 +1,51 @@
-import { House, History, Settings, Sun } from 'lucide-react'
-import styles from './styles.module.css'
+import { House, History, Settings, Sun } from "lucide-react";
+import styles from "./styles.module.css";
+import { useState } from "react";
+
+type AvailableThemes = "dark" | "light";
 
 export function Menu() {
-    return (
-        <nav className={styles.menu}>
-            <a className={styles.menuLink} href="">
-                <House />
-            </a>
-            <a className={styles.menuLink} href="">
-                <History />
-            </a>
-            <a className={styles.menuLink} href="">
-                <Settings />
-            </a>
-            <a className={styles.menuLink} href="">
-                <Sun />
-            </a>
-        </nav>
-    )
+  const [theme, setTheme] = useState<AvailableThemes>("dark");
+
+  function handleThemeChange(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+    event.preventDefault();
+  }
+
+  return (
+    <nav className={styles.menu}>
+      <a
+        className={styles.menuLink}
+        href=""
+        aria-label="Ir para a Home"
+        title="Ir para a Home"
+      >
+        <House />
+      </a>
+      <a
+        className={styles.menuLink}
+        href=""
+        aria-label="Ver Histórico"
+        title="Ver Histórico"
+      >
+        <History />
+      </a>
+      <a
+        className={styles.menuLink}
+        href=""
+        aria-label="Configurações"
+        title="Configurações"
+      >
+        <Settings />
+      </a>
+      <a
+        className={styles.menuLink}
+        href=""
+        aria-label="Mudar Tema"
+        title="Mudar Tema"
+        onClick={handleThemeChange}
+      >
+        <Sun />
+      </a>
+    </nav>
+  );
 }
