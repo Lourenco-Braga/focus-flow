7026c5c507a4d5bbd1e960cc3ca6f190fa743395
Lourenço Braga <lbraganeto2003@gmail.com>
2026-07-16 11:35:55 -0400

    props state by many components

--- src/components/Countdown/index.tsx
+++ src/components/Countdown/index.tsx
@@ -1,9 +1,10 @@
 import styles from './styles.module.css'
+import type { HomeProps } from '../../pages/Home'
 
-export function Countdown() {
+export function Countdown({ state }: HomeProps) {
     return (
         <div className={styles.container}>
-            00:00
+            {state.formattedSecondsRemaining}
         </div>
     )
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,25 @@
 import { Container } from "../../components/Container";
 import { Countdown } from "../../components/Countdown";
 import { MainForm } from "../../components/MainForm";
+import type { TaskStateModel } from "../../models/TaskStateModel";
 import { MainTemplate } from "../../templates/MainTemplate";
 
-export function Home() {
+export type HomeProps = {
+  state: TaskStateModel,
+  setState: React.Dispatch<React.SetStateAction<TaskStateModel>>
+}
+
+
+export function Home(props: HomeProps) {
+
   return (
     <MainTemplate>
         <Container>
-            <Countdown />
+            <Countdown {...props} />
         </Container>
 
         <Container>
-            <MainForm />
+            <MainForm {...props} />
         </Container>
     </MainTemplate>
    
